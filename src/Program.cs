700271e0b6f8c8c6cc1f59728217f2a02c8f1067
leon.mbs@gmail.com <leon.mbs@gmail.com>
2024-10-23 12:42:44 +0300

    init

---
+++ src/Program.cs
@@ -0,0 +1,82 @@
+ 
+using System.Text;
+using System.Text.Json;
+ 
+
+namespace PServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            Console.WriteLine("Statrted...");
+            builder.Services.AddCors();
+
+            var app = builder.Build();
+
+            var PrinterName = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("AppSettings")["PrinterName"];
+
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            app.MapPost("/print", async (HttpContext httpContext) =>
+            {
+                ESC_POS_USB_NET.Printer.Printer printer = new ESC_POS_USB_NET.Printer.Printer(PrinterName, "cp866");
+
+
+                var jsonString = "";
+                var req = httpContext.Request;
+                try { 
+                    using (StreamReader reader = new StreamReader(req.Body, Encoding.UTF8, true))
+                    {
+                        jsonString = await reader.ReadToEndAsync();
+                    }
+
+                    var list = JsonSerializer.Deserialize<List<byte>>(jsonString);
+
+                    printer.Append(list.ToArray());
+                    printer.PrintDocument();
+                    return "";
+
+
+
+                }catch(Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+
+                    return e.Message;
+                }
+            });
+
+            app.MapGet("/testprint", async (HttpContext httpContext) =>
+            {
+                try
+                {
+                    ESC_POS_USB_NET.Printer.Printer printer = new ESC_POS_USB_NET.Printer.Printer(PrinterName, "cp866");
+
+                    printer.Append("Test printer");
+                    printer.Append("Тест принтера");
+                    printer.Append(new byte[] { 0x0a });
+                    printer.PrintDocument();
+                    printer.Clear();
+                    Console.WriteLine("Тeст принтера");
+                    return "Тeст принтера";
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+
+                    return e.Message;
+                }
+            });
+
+
+            app.UseCors(builder => builder.AllowAnyOrigin());
+            app.Run();
+        }
+    }
+}
---
+++ src/lib/Epson Commands/Image.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System;
+using System.IO;
+using ESC_POS_USB_NET.Interfaces.Command;
+using System.Drawing;
+
+namespace ESC_POS_USB_NET.EpsonCommands
+{
+  
+    public class BitmapData
+    {
+        public BitArray Dots { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+}
+
