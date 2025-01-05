fbeb774e768ad54fffba2bf1af365ae1a87dfa0f
leon.mbs@gmail.com <leon.mbs@gmail.com>
2025-01-05 13:21:19 +0200

    fix

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,9 @@
- 
+п»ї 
 using System.Text;
 using System.Text.Json;
  
 
-namespace PServer
+namespace PrintServer
 {
     public class Program
     {
@@ -12,6 +12,9 @@ namespace PServer
             var builder = WebApplication.CreateBuilder(args);
 
             Console.WriteLine("Statrted...");
+            string docPath= AppContext.BaseDirectory ;
+ 
+
             builder.Services.AddCors();
 
             var app = builder.Build();
@@ -20,13 +23,6 @@ namespace PServer
 
 
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-
-            //проверка  что  запущке
-            app.MapGet("/check", async (HttpContext httpContext) =>
-            {
-                return "POS server: OK";
-            });
 
             app.MapPost("/print", async (HttpContext httpContext) =>
             {
@@ -39,7 +35,7 @@ namespace PServer
                     using (StreamReader reader = new StreamReader(req.Body, Encoding.UTF8, true))
                     {
                         jsonString = await reader.ReadToEndAsync();
-                    }
+                     }
 
                     var list = JsonSerializer.Deserialize<List<byte>>(jsonString);
 
@@ -51,27 +47,29 @@ namespace PServer
 
                 }catch(Exception e)
                 {
+                    Console.WriteLine("\r\n");
 
                     Console.WriteLine(e.Message);
+             
+                    File.WriteAllText(Path.Combine(docPath, "errorlog.txt"), e.Message+"\r\n"+jsonString);
 
                     return e.Message;
                 }
             });
 
-            //тест  с  браузера
-            app.MapGet("/testprint", async (HttpContext httpContext) =>
+            app.MapGet("/test", async (HttpContext httpContext) =>
             {
                 try
                 {
                     ESC_POS_USB_NET.Printer.Printer printer = new ESC_POS_USB_NET.Printer.Printer(PrinterName, "cp866");
 
                     printer.Append("Test printer");
-                    printer.Append("Тест принтера");
+                    printer.Append("РўРµСЃС‚ РїСЂРёРЅС‚РµСЂР°");
                     printer.Append(new byte[] { 0x0a });
                     printer.PrintDocument();
                     printer.Clear();
-                    Console.WriteLine("Тeст принтера");
-                    return "Тeст принтера";
+                    Console.WriteLine("РўeСЃС‚ РїСЂРёРЅС‚РµСЂР°");
+                    return "РўeСЃС‚ РїСЂРёРЅС‚РµСЂР°";
                 }
                 catch (Exception e)
                 {
@@ -81,22 +79,18 @@ namespace PServer
                     return e.Message;
                 }
             });
-            //тест  с  браузера для  режима label
-            app.MapGet("/testprintlabel", async (HttpContext httpContext) =>
+            app.MapGet("/label", async (HttpContext httpContext) =>
             {
                 try
                 {
                     ESC_POS_USB_NET.Printer.Printer printer = new ESC_POS_USB_NET.Printer.Printer(PrinterName, "cp866");
-                   
-                   
-                    printer.Append("CLS");
-                    printer.Append("DIRECTION 1");
+             
                     printer.Append("TEXT 10,10,\"2\",0,1,1,\"Test printer\"");
                     printer.Append("PRINT 1,1");
                     printer.PrintDocument();
                     printer.Clear();
-                    Console.WriteLine("Тeст принтера");
-                    return "Тeст принтера";
+                    Console.WriteLine("РўeСЃС‚ РїСЂРёРЅС‚РµСЂР°");
+                    return "РўeСЃС‚ РїСЂРёРЅС‚РµСЂР°";
                 }
                 catch (Exception e)
                 {
@@ -107,99 +101,9 @@ namespace PServer
                 }
             });
 
-            //вкс  с  вксов
-            app.MapGet("/scale", async (HttpContext httpContext) =>
-            {
-               
-                var req = httpContext.Request;
-                try
-                {
-                     
 
-                    double weight = 0;   //gtodo  получаем вес с весовd
-
-                    var resp = new RespData {
-                        success = true,
-                        weight= weight
-                    };
-                    return JsonSerializer.Serialize(resp);
-
-
-
-                }
-                catch (Exception e)
-                {
-
-                    Console.WriteLine(e.Message);
-
-                    var resp = new RespData
-                    {
-                        success = false,
-                        error= e.Message
-                    };
-                    return JsonSerializer.Serialize(resp);
-                }
-            });
-
-             
-            app.MapPost("/bankpay", async (HttpContext httpContext) =>
-            {
-                var reqdata = new ReqBankData();
-                var req = httpContext.Request;
-                try
-                {
-                    using (StreamReader reader = new StreamReader(req.Body, Encoding.UTF8, true))
-                    {
-                        var jsonString = await reader.ReadToEndAsync();
-                        reqdata = JsonSerializer.Deserialize<ReqBankData>(jsonString);
-                    }
-
-
-                    double weight = 55;   //gtodo  получаем вес с весовd
-
-                    var resp = new RespData
-                    {
-                        success = true,
-                        weight = weight
-                    };
-                    return JsonSerializer.Serialize(resp);
-
-
-
-                }
-                catch (Exception e)
-                {
-
-                    Console.WriteLine(e.Message);
-
-                    var resp = new RespData
-                    {
-                        success = false,
-                        error = e.Message
-                    };
-                    return JsonSerializer.Serialize(resp);
-                }
-            });
-           
             app.UseCors(builder => builder.AllowAnyOrigin());
             app.Run();
         }
     }
-
-    public  class ReqBankData
-    {
-        public string mfo { get; set; }
-        public double amount { get; set; }
-        public string account { get; set; }
-
-    }
-    public class RespData
-    {
-        public bool success { get; set; }  //true если  нет ощибок
-        public string? error { get; set; }  //сообщение об ошиьке
-        public int? tranid { get; set; }   //вес  с электронных весов
-        public double? weight { get; set; }   //вес  с электронных весов
-
-    }
-
 }
